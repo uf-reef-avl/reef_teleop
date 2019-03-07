c1c4fe33ac587671971d94c43d354e2b73565ed4
Prashant Ganesh <prashant.ganesh@colorado.edu>
2019-03-07 11:53:29 -0600

    added reef_msgs dep

--- include/Teleop.h
+++ include/Teleop.h
@@ -4,6 +4,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <rosflight_msgs/RCRaw.h>
+#include <reef_msgs/DesiredState.h>
 
 namespace reef_teleop {
     struct Axis
@@ -19,8 +20,9 @@ namespace reef_teleop {
     private:
         ros::NodeHandle private_nh_;
         ros::NodeHandle node_handle_;
-        ros::Subscriber joy_subscriber_, rc_subscriber_;
-        ros::Publisher attitude_publisher_, altitude_publisher_, velocity_publisher_;
+
+        ros::Subscriber joy_subscriber_;
+        ros::Publisher desired_state_publisher_;
 
         struct {
             Axis x;
@@ -39,17 +41,15 @@ namespace reef_teleop {
 
         double zCommand, zCommandMin, zCommandMax, zScale;
         bool invertX;
-
         bool rcSwitchFlipped;
         int rcChannel;
-        ros::Time attCmdStart;
+        std::string control_mode;
 
     public:
         Teleop();
-        void rcRawCallback(const rosflight_msgs::RCRawConstPtr &msg);
         void joyAttAltCallback(const sensor_msgs::JoyConstPtr &joy);
-        void joyVelAltCallback(const sensor_msgs::JoyConstPtr &joy);
-        void joyXBtnAttAltCallback(const sensor_msgs::JoyConstPtr &joy);
+        void joyCallback(const sensor_msgs::JoyConstPtr &joy);
+
     };
 
     double getAxis(const sensor_msgs::JoyConstPtr &joy, const Axis &axis);
--- src/Teleop.cpp
+++ src/Teleop.cpp
@@ -1,300 +1,114 @@
-#include <reef_teleop/AttitudeCommand.h>
-#include <reef_teleop/AltitudeCommand.h>
-#include <reef_teleop/VelocityCommand.h>
-
 #include "Teleop.h"
 
 namespace reef_teleop {
 
-    Teleop::Teleop() : private_nh_("~") {
-        //Controller layout parameters - defaults to xbox layout
-        private_nh_.param<int>("x_axis", axes.x.axis, 4);
-        private_nh_.param<int>("y_axis", axes.y.axis, 5);
-        private_nh_.param<int>("z_axis", axes.z.axis, 2);
-        private_nh_.param<int>("thrust_axis", axes.thrust.axis, 2);
-        private_nh_.param<int>("yaw_axis", axes.yaw.axis, 1);
+Teleop::Teleop() : private_nh_("~") {
+    //Controller layout parameters - defaults to xbox layout
+    private_nh_.param<int>("x_axis", axes.x.axis, 4);
+    private_nh_.param<int>("y_axis", axes.y.axis, 5);
+    private_nh_.param<int>("z_axis", axes.z.axis, 2);
+    private_nh_.param<int>("thrust_axis", axes.thrust.axis, 2);
+    private_nh_.param<int>("yaw_axis", axes.yaw.axis, 1);
 
-        private_nh_.param<int>("a_btn", buttons.a, 1);
-        private_nh_.param<int>("b_btn", buttons.b, 2);
-        private_nh_.param<int>("x_btn", buttons.x, 3);
-        private_nh_.param<int>("y_btn", buttons.y, 4);
+    private_nh_.param<int>("a_btn", buttons.a, 1);
+    private_nh_.param<int>("b_btn", buttons.b, 2);
+    private_nh_.param<int>("x_btn", buttons.x, 3);
+    private_nh_.param<int>("y_btn", buttons.y, 4);
 
-        private_nh_.param<double>("initial_z_cmd", zCommand, 0);
-        private_nh_.param<double>("min_z_cmd", zCommandMin, -10.0);
-        private_nh_.param<double>("max_z_cmd", zCommandMax, 10.0);
-        private_nh_.param<double>("z_scale", zScale, 0.1);
+    private_nh_.param<double>("initial_z_cmd", zCommand, 0);
+    private_nh_.param<double>("min_z_cmd", zCommandMin, -10.0);
+    private_nh_.param<double>("max_z_cmd", zCommandMax, 10.0);
+    private_nh_.param<double>("z_scale", zScale, 0.1);
+    private_nh_.param<bool>("invert_x", invertX, false);
 
-        private_nh_.param<bool>("invert_x", invertX, false);
+    private_nh_.param<double>("yawrate_max", axes.yaw.max, 0.5);
+    //Set zmax (speed at which position changes)
+    axes.z.max = 0.05;
 
+    joy_subscriber_ =
+        node_handle_.subscribe<sensor_msgs::Joy>("joy", 10, boost::bind(&Teleop::joyCallback, this, _1));
 
-        //Control mode parameter
-        std::string control_mode;
-        private_nh_.param<std::string>("control_mode", control_mode, "attitude_altitude");
+    desired_state_publisher_ = node_handle_.advertise<reef_msgs::DesiredState>("desired_state", 1);
 
-        if (control_mode == "attitude_altitude") {
-            ROS_INFO_STREAM("Attitude + Altitude Hold Mode Enabled");
-            
-            //Limit parameters
-            private_nh_.param<double>("yawrate_max", axes.yaw.max, 0.5);
-            private_nh_.param<double>("pitch_max", axes.x.max, 0.15);
-            private_nh_.param<double>("roll_max", axes.y.max, 0.15);
+    //Control mode parameter
+    private_nh_.param<std::string>("control_mode", control_mode, "attitude_altitude");
 
-
-            joy_subscriber_ = node_handle_.subscribe<sensor_msgs::Joy>("joy", 10, boost::bind(&Teleop::joyAttAltCallback, this, _1));
-            attitude_publisher_ = node_handle_.advertise<AttitudeCommand>("/teleop_command/attitude", 1);
-            altitude_publisher_ = node_handle_.advertise<AltitudeCommand>("/teleop_command/altitude", 1, true);
-
-            //Set zmax (speed at which position changes)
-            axes.z.max = 0.05;
-        } else if (control_mode == "velocity_altitude") {
-            ROS_INFO_STREAM("Velocity + Altitude Control Mode Enabled");
-
-            //Limit parameters
-            private_nh_.param<double>("x_dot_max", axes.x.max, 0.25);
-            private_nh_.param<double>("y_dot_max", axes.y.max, 0.25);
-            private_nh_.param<double>("yawrate_max", axes.yaw.max, 0.5);
-
-            joy_subscriber_ = node_handle_.subscribe<sensor_msgs::Joy>("joy", 10, boost::bind(&Teleop::joyVelAltCallback, this, _1));
-            velocity_publisher_ = node_handle_.advertise<VelocityCommand>("/teleop_command/velocity", 1);
-            altitude_publisher_ = node_handle_.advertise<AltitudeCommand>("/teleop_command/altitude", 1, true);
-
-            //Set zmax (speed at which position changes)
-            axes.z.max = 0.05;
-        } else if (control_mode == "x_btn_attitude_altitude") {
-            ROS_INFO_STREAM("X-button Attitude + Altitude Control Mode Enabled");
-
-            private_nh_.param<double>("pitch_max", axes.x.max, 0.15);
-            joy_subscriber_ = node_handle_.subscribe<sensor_msgs::Joy>("joy", 10, boost::bind(&Teleop::joyXBtnAttAltCallback, this, _1));
-            attitude_publisher_ = node_handle_.advertise<AttitudeCommand>("/teleop_command/attitude", 1);
-            altitude_publisher_ = node_handle_.advertise<AltitudeCommand>("/teleop_command/altitude", 1, true);
-
-            //Set zmax (speed at which z position changes)
-            axes.z.max = 0.05;
-        } else if (control_mode == "dist_accel_altitude") {
-            ROS_INFO_STREAM("Distributed Accel Attitude Control Mode Enabled");
-
-            rcSwitchFlipped = false;
-            private_nh_.param<int>("rc_channel", rcChannel, 6);
-            rc_subscriber_ = node_handle_.subscribe<rosflight_msgs::RCRaw>("rc_raw", 1, boost::bind(&Teleop::rcRawCallback, this, _1));
-
-            attitude_publisher_ = node_handle_.advertise<AttitudeCommand>("/teleop_command/attitude", 1);
-            altitude_publisher_ = node_handle_.advertise<AltitudeCommand>("/teleop_command/altitude", 1, true);
-
-
-        if (control_mode.find("altitude") != std::string::npos) {
-            //Publish initial altitude
-            AltitudeCommand altCmd;
-            altCmd.header.stamp = ros::Time::now();
-            altCmd.z = zCommand;
-            altitude_publisher_.publish(altCmd);
-        }
+    if (control_mode == "attitude_altitude") {
+        ROS_INFO_STREAM("Attitude + Altitude Hold Mode Enabled");
+        //Limit parameters
+        private_nh_.param<double>("pitch_max", axes.x.max, 0.15);
+        private_nh_.param<double>("roll_max", axes.y.max, 0.15);
+    } else if (control_mode == "velocity_altitude") {
+        ROS_INFO_STREAM("Velocity + Altitude Control Mode Enabled");
+        //Limit parameters
+        private_nh_.param<double>("x_dot_max", axes.x.max, 0.25);
+        private_nh_.param<double>("y_dot_max", axes.y.max, 0.25);
     }
+}
+
+
+void Teleop::joyCallback(const sensor_msgs::JoyConstPtr &joy) {
+
+    reef_msgs::DesiredState desired_state_msg;
+
+    desired_state_msg.header.stamp = ros::Time::now();
+    //Populate and publish altitude command
+    zCommand -= zScale*getAxis(joy, axes.z);
+    if (zCommand < zCommandMin) {
+        zCommand = zCommandMin;
+    } else if (zCommand > zCommandMax) {
+        zCommand = zCommandMax;
     }
 
+    if (control_mode == "attitude_altitude") {
 
-    void Teleop::rcRawCallback(const rosflight_msgs::RCRawConstPtr &msg) {
-        if (rcSwitchFlipped && msg->values[rcChannel] <= 1500)
-        {
-            rcSwitchFlipped = false;
-            ROS_WARN_STREAM("Attitude command stream deactivated");
-        }
-        else if (!rcSwitchFlipped && msg->values[rcChannel] > 1500)
-        {
-            rcSwitchFlipped = true;
-            attCmdStart = ros::Time::now();
-            ROS_WARN_STREAM("Attitude command stream activated");
-        }
-
-        AttitudeCommand attCmd;
-        AltitudeCommand altCmd;
-        attCmd.header.stamp = altCmd.header.stamp = ros::Time::now();
-        attCmd.pitch = 0;
-        attCmd.yaw_rate = 0;
-        if (rcSwitchFlipped) {
-            double secsSinceStart = (attCmd.header.stamp - attCmdStart).toSec();
-            double multiplier = 1.0;
-
-            if (secsSinceStart < 1.5) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 3.5) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 5.0) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 6.5) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 8.0) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 10.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 12.0) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 13.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 14.0) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 15.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 16.0) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 17.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 17.5) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 18.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 18.5) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 19.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 19.25) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 19.5) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 19.75) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 20.0) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 20.125) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 20.25) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 20.375) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 20.5) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 20.6) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 20.7) {
-                multiplier = -1.0;
-            } else if (secsSinceStart < 20.8) {
-                multiplier = 1.0;
-            } else if (secsSinceStart < 20.9) {
-                multiplier = -1.0;
-            }
-
-            else {
-                multiplier = 0.0;
-                ROS_WARN_ONCE("Attitude command stream completed");
-            }
-            
-            attCmd.roll = 0.3 * multiplier;
-
-            //ROS_ERROR_STREAM(secsSinceStart << ": " << attCmd.roll);
-        } else {
-            attCmd.roll = 0;
-        }
-        attitude_publisher_.publish(attCmd);
-
-
-        altCmd.z = zCommand;
-        altitude_publisher_.publish(altCmd);
-    }
-
-    void Teleop::joyAttAltCallback(const sensor_msgs::JoyConstPtr &joy) {
-        AttitudeCommand attCmd;
-        AltitudeCommand altCmd;
-
-        //Populate and publish attitude command
-        attCmd.header.stamp = altCmd.header.stamp = ros::Time::now();
-        attCmd.roll = getAxis(joy, axes.x);
+        desired_state_msg.attitude_valid = true;
+        desired_state_msg.attitude.x = getAxis(joy, axes.x);
         if (!invertX) {
-            attCmd.roll = -1.0*attCmd.roll;
+            desired_state_msg.attitude.x = -1.0*desired_state_msg.attitude.x;
         }
-        attCmd.pitch = -1.0*getAxis(joy, axes.y);
-        attCmd.yaw_rate = getAxis(joy, axes.yaw);
+        desired_state_msg.attitude.y = -1.0*getAxis(joy, axes.y);
+        desired_state_msg.attitude.yaw = getAxis(joy, axes.yaw);
         if (!invertX) {
-            attCmd.yaw_rate = -1.0*attCmd.yaw_rate;
+            desired_state_msg.attitude.yaw = -1.0*desired_state_msg.attitude.yaw;
         }
+        desired_state_msg.attitude.z = zCommand;
+    }
+    else if (control_mode == "velocity_altitude") {
+        desired_state_msg.velocity_valid = true;
+        desired_state_msg.velocity.x = getAxis(joy, axes.y);
+        desired_state_msg.velocity.y = -1.0*getAxis(joy, axes.x);
+        desired_state_msg.velocity.yaw = -1.0*getAxis(joy, axes.yaw);
+        desired_state_msg.velocity.z = zCommand;
+    }
+    desired_state_publisher_.publish(desired_state_msg);
+}
 
-        attitude_publisher_.publish(attCmd);
 
-        //Populate and publish altitude command
-        zCommand -= zScale*getAxis(joy, axes.z);
-        if (zCommand < zCommandMin) {
-            zCommand = zCommandMin;
-        } else if (zCommand > zCommandMax) {
-            zCommand = zCommandMax;
-        }
-        altCmd.z = zCommand;
-        altitude_publisher_.publish(altCmd);
+double getAxis(const sensor_msgs::JoyConstPtr &joy, const Axis &axis) {
+    //Axis out of range
+    if (axis.axis == 0 || std::abs(axis.axis) > joy->axes.size()) {
+        ROS_ERROR_STREAM("Axis " << axis.axis << " out of range, joy has " << joy->axes.size() << " axes");
+        return 0;
     }
 
-    void Teleop::joyVelAltCallback(const sensor_msgs::JoyConstPtr &joy) {
-        VelocityCommand velCmd;
-        AltitudeCommand altCmd;
+    double output = std::abs(axis.axis) / axis.axis * joy->axes[std::abs(axis.axis) - 1] * axis.max + axis.offset;
 
-        //Populate and publish attitude command
-        velCmd.header.stamp = altCmd.header.stamp = ros::Time::now();
-        velCmd.y_dot = -1.0*getAxis(joy, axes.x);
-        velCmd.x_dot = getAxis(joy, axes.y);
-        velCmd.yaw_rate = -1.0*getAxis(joy, axes.yaw);
-        velocity_publisher_.publish(velCmd);
-
-        //Populate and publish altitude command
-        zCommand -= zScale*getAxis(joy, axes.z);
-        if (zCommand < zCommandMin) {
-            zCommand = zCommandMin;
-        } else if (zCommand > zCommandMax) {
-            zCommand = zCommandMax;
-        }
-        altCmd.z = zCommand;
-        altitude_publisher_.publish(altCmd);
+    //5% stick deadzone
+    if (std::abs(output) < axis.max * 0.05) {
+        output = 0.0;
     }
 
-    void Teleop::joyXBtnAttAltCallback(const sensor_msgs::JoyConstPtr &joy) {
-        AttitudeCommand attCmd;
-        AltitudeCommand altCmd;
+    return output;
+}
 
-        //Populate and publish attitude command
-        attCmd.header.stamp = altCmd.header.stamp = ros::Time::now();
-        attCmd.roll = 0;
-        if (getButton(joy, buttons.a)) {
-            attCmd.pitch = -1.0*fabs(axes.x.max);
-        } else {
-            attCmd.pitch = 0;
-        }
-        attCmd.yaw_rate = getAxis(joy, axes.yaw);
-        if (!invertX) {
-            attCmd.yaw_rate = -1.0*attCmd.yaw_rate;
-        }
-        attitude_publisher_.publish(attCmd);
-
-        //Populate and publish altitude command
-        zCommand -= zScale*getAxis(joy, axes.z);
-        if (zCommand < zCommandMin) {
-            zCommand = zCommandMin;
-        } else if (zCommand > zCommandMax) {
-            zCommand = zCommandMax;
-        }
-        altCmd.z = zCommand;
-        altitude_publisher_.publish(altCmd);
+bool getButton(const sensor_msgs::JoyConstPtr &joy, const int button) {
+    if (button <= 0 || button > joy->buttons.size()) {
+        ROS_ERROR_STREAM("Button " << button << " out of range, joy has " << joy->buttons.size() << " buttons");
+        return false;
     }
 
-
-    double getAxis(const sensor_msgs::JoyConstPtr &joy, const Axis &axis) {
-        //Axis out of range
-        if (axis.axis == 0 || std::abs(axis.axis) > joy->axes.size()) {
-            ROS_ERROR_STREAM("Axis " << axis.axis << " out of range, joy has " << joy->axes.size() << " axes");
-            return 0;
-        }
-
-        double output = std::abs(axis.axis) / axis.axis * joy->axes[std::abs(axis.axis) - 1] * axis.max + axis.offset;
-
-        //5% stick deadzone
-        if (std::abs(output) < axis.max * 0.05) {
-            output = 0.0;
-        }
-
-        return output;
-    }
-
-    bool getButton(const sensor_msgs::JoyConstPtr &joy, const int button) {
-        if (button <= 0 || button > joy->buttons.size()) {
-            ROS_ERROR_STREAM("Button " << button << " out of range, joy has " << joy->buttons.size() << " buttons");
-            return false;
-        }
-
-        return joy->buttons[button - 1] > 0;
-    }
+    return joy->buttons[button - 1] > 0;
+}
 
 }
